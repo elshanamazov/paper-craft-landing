f33ac22001e06d62d331fdb825d8dd3ecf33cf13
elshanamazov <elshanamazov@gmail.com>
2024-03-24 22:33:19 +0400

    added favicon

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,143 @@
-(()=>{"use strict";!function(){const e=document.querySelector(".burger"),t=document.querySelector(".nav"),s=document.querySelector("body");e.addEventListener("click",(()=>{t.classList.contains("active")?(t.classList.remove("active"),e.classList.remove("burger--active"),s.classList.remove("locked")):(t.classList.add("active"),e.classList.add("burger--active"),s.classList.add("locked"))})),window.addEventListener("resize",(()=>{window.innerWidth>991.98&&(t.classList.remove("active"),e.classList.remove("burger--active"),s.classList.remove("locked"))}))}(),function(){const e=document.querySelector(".search-btn"),t=document.querySelector(".search-form__close"),s=document.querySelector(".search-form");e.addEventListener("click",(function(e){e.preventDefault(),s.classList.add("search-form--open"),s.setAttribute("aria-expanded","true")})),t.addEventListener("click",(function(e){e.preventDefault(),s.classList.remove("search-form--open"),s.setAttribute("aria-expanded","false")}))}()})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/components/burger.js":
+/*!*************************************!*\
+  !*** ./src/js/components/burger.js ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   burgerMenu: () => (/* binding */ burgerMenu)
+/* harmony export */ });
+function burgerMenu() {
+  const burger = document.querySelector(".burger");
+  const menu = document.querySelector(".nav");
+  const body = document.querySelector("body");
+  burger.addEventListener("click", () => {
+    if (!menu.classList.contains("active")) {
+      menu.classList.add("active");
+      burger.classList.add("burger--active");
+      body.classList.add("locked");
+    } else {
+      menu.classList.remove("active");
+      burger.classList.remove("burger--active");
+      body.classList.remove("locked");
+    }
+  });
+  // resize
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 991.98) {
+      menu.classList.remove("active");
+      burger.classList.remove("burger--active");
+      body.classList.remove("locked");
+    }
+  });
+}
+
+/***/ }),
+
+/***/ "./src/js/components/search.js":
+/*!*************************************!*\
+  !*** ./src/js/components/search.js ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   search: () => (/* binding */ search)
+/* harmony export */ });
+function search() {
+  const searchIcon = document.querySelector(".search-btn");
+  const closeSearch = document.querySelector(".search-form__close");
+  const searchField = document.querySelector(".search-form");
+  searchIcon.addEventListener("click", function (e) {
+    e.preventDefault();
+    searchField.classList.add("search-form--open");
+    searchField.setAttribute("aria-expanded", "true");
+  });
+  closeSearch.addEventListener("click", function (e) {
+    e.preventDefault();
+    searchField.classList.remove("search-form--open");
+    searchField.setAttribute("aria-expanded", "false");
+  });
+}
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _components_burger__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/burger */ "./src/js/components/burger.js");
+/* harmony import */ var _components_search__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./components/search */ "./src/js/components/search.js");
+
+
+(0,_components_burger__WEBPACK_IMPORTED_MODULE_0__.burgerMenu)();
+(0,_components_search__WEBPACK_IMPORTED_MODULE_1__.search)();
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
